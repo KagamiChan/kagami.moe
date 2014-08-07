5977d43f4dd64d2b632c771c943460cdf462799d
Seki Kagami <moe.pascal@gmail.com>
2014-08-07 22:51:41 +0800

    Minor improvements: -   a logo when the top banner is invisible -   add virtual #top to better locate -   viewport and vh,vw to enhance mobile exp

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $('nav').stick_in_parent()
 
-    $(window).scroll(function(){
-        var scrollTop = $(window).scrollTop()
-        if(scrollTop>145)
-            $('nav').css({"border-bottom-width":"1px"});
-        else
-            $('nav').css("border-bottom-width","0px");
-    })
+    // $(window).scroll(function(){
+    //     var scrollTop = $(window).scrollTop()
+    //     if(scrollTop>145)
+    //         $('nav').css({"border-bottom-width":"1px"});
+    //     else
+    //         $('nav').css("border-bottom-width","0px");
+    // })
 })
